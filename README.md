20ab1d02a7219aac849b54b6600667e956061227
mathrews <mateusps4gg@gmail.com>
2024-07-03 11:23:14 -0300

    adding link for notion documentation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LEARNING CPP:
 
 - This repository is for safe my learn of this language and other tools.
+
+- Link for Notion documentation: https://www.notion.so/C-Documentation-4b0b8e2d32614aab83b87d3d371e6e2f?pvs=4
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+// directive for pre-processor to give the iostream tools
 #include <iostream>
-
-#include "header.h"
+// directive for pre-processor to give the header tools made by myself
+#include "src/header.h"
 
 int main() {
+    // stream cout receiving a string literal as entity fo execute some print in console
     std::cout << "Hello, World!" << std::endl;
     exercise();
 
